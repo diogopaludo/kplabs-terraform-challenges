a7cb3aead72b0a276fcc3adf8b490031388cae7e
Diogo Paludo de Oliveira <diogo.paludodeoliveira@gmail.com>
2024-05-08 23:29:23 -0300

    Challenge 3 done.

--- challenge-3/main.tf
+++ challenge-3/main.tf
@@ -4,6 +4,12 @@ variable "instance_config" {
   default = {
     instance1 = { instance_type = "t2.micro", ami = "ami-03a6eaae9938c858c" }
     instance2 = { instance_type = "t2.small", ami = "ami-053b0d53c279acc90" }
+    instance3 = { instance_type = "t2.large", ami = "ami-053b0d53c279acc90" }
   }
 }
 
+resource "aws_instance" "instances" {
+  for_each = var.instance_config
+  ami = each.value.ami
+  instance_type = each.value.instance_type
+}
